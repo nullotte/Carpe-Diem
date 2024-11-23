371d9bfe58ed7960c672bd09f60b1b49e810db53
Chawanya-c <kayayeare16@gmail.com>
2024-11-23 22:22:36 +0700

    storage

--- src/carpediem/content/blocks/CDStorage.java
+++ src/carpediem/content/blocks/CDStorage.java
@@ -5,11 +5,17 @@ import carpediem.content.*;
 import carpediem.world.blocks.storage.*;
 import carpediem.world.draw.*;
 import mindustry.type.*;
+import mindustry.world.*;
 import mindustry.world.blocks.storage.*;
 import mindustry.world.draw.*;
 
 public class CDStorage {
-    public static CoreBlock landingPodT0, landingPodT1, landingPodT2, industryHub;
+    public static CoreBlock
+    landingPodT0, landingPodT1, landingPodT2,
+    industryHub;
+
+    public static Block
+    storageVault, storageRelay;
 
     public static void load() {
         landingPodT0 = new LandingPod("landing-pod-t0") {{
@@ -47,6 +53,21 @@ public class CDStorage {
                     new DrawDefault(),
                     new DrawTeam()
             );
+        }};
+
+        storageVault = new StorageBlock("storage-vault") {{
+            requirements(Category.effect, ItemStack.with());
+            size = 5;
+            itemCapacity = 5000;
+        }};
+
+        storageRelay = new StorageRelay("storage-relay") {{
+            requirements(Category.effect, ItemStack.with());
+            size = 5;
+
+            drawer = new DrawDefault();
+
+            consumePower(2f);
         }};
     }
 }
