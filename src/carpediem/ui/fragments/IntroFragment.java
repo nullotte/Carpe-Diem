f501043d20e71d3be2fbd1ff5159ce36d7d1c5c9
nullotte <kayayeare16@gmail.com>
2025-02-03 14:59:58 +0700

    intro cutscene tweaks

--- src/carpediem/ui/fragments/IntroFragment.java
+++ src/carpediem/ui/fragments/IntroFragment.java
@@ -4,6 +4,8 @@ import arc.*;
 import arc.flabel.*;
 import arc.scene.*;
 import arc.scene.actions.*;
+import arc.scene.event.*;
+import arc.scene.ui.*;
 import arc.util.*;
 import carpediem.graphics.*;
 import mindustry.*;
@@ -11,11 +13,13 @@ import mindustry.core.GameState.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.ui.*;
+import mindustry.world.blocks.storage.CoreBlock.*;
 
 public class IntroFragment {
     public static final float duration = 10f * 60f, spawnDelay = 60f;
 
     public float progress;
+    public boolean removedLanding;
 
     public void build(Group parent) {
         parent.fill(Styles.black, t -> {
@@ -49,6 +53,20 @@ public class IntroFragment {
             }).grow().center();
 
             t.update(() -> {
+                if (!removedLanding && !Core.settings.getBool("skipcoreanimation")) {
+                    Image front = (Image) Core.scene.find(e -> e.parent == Core.scene.root && e instanceof Image && e.touchable == Touchable.disabled && e.fillParent);
+
+                    if (front != null) {
+                        front.remove();
+                        removedLanding = true;
+                    }
+                }
+
+                CoreBuild core = Vars.player.team().core();
+                if (core != null) {
+                    Core.camera.position.set(core);
+                }
+
                 if (!Vars.state.isPaused()) {
                     if (Vars.state.isMenu()) {
                         t.remove();
