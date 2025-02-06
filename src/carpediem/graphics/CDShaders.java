2a7cca31991828b4c068a4f5dff2c2320f9fae9f
nullotte <kayayeare16@gmail.com>
2025-02-06 22:48:30 +0700

    glenn if you're seeing this somehow feel free to yell at me in my dms

--- src/carpediem/graphics/CDShaders.java
+++ src/carpediem/graphics/CDShaders.java
@@ -1,6 +1,7 @@
 package carpediem.graphics;
 
 import arc.files.*;
+import arc.graphics.gl.*;
 import carpediem.graphics.shaders.*;
 import mindustry.*;
 
@@ -9,8 +10,14 @@ public class CDShaders {
     public static DepthAtmosphereShader depthAtmosphere;
 
     public static void load() {
+        String prevVert = Shader.prependVertexCode, prevFrag = Shader.prependFragmentCode;
+        Shader.prependVertexCode = Shader.prependFragmentCode = "";
+
         depth = new DepthShader();
         depthAtmosphere = new DepthAtmosphereShader();
+
+        Shader.prependVertexCode = prevVert;
+        Shader.prependFragmentCode = prevFrag;
     }
 
     public static Fi file(String name) {
