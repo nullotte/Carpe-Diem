fd8077207739e255223c94eb81486aad88c51e86
nullotte <kayayeare16@gmail.com>
2025-02-21 22:44:40 +0700

    I CANT BE BOTHERED AAHGHJGJHGFJ

---
+++ src/carpediem/world/blocks/campaign/LandingPodLoader.java
@@ -0,0 +1,9 @@
+package carpediem.world.blocks.campaign;
+
+import mindustry.world.blocks.payloads.*;
+
+public class LandingPodLoader extends PayloadBlock {
+    public LandingPodLoader(String name) {
+        super(name);
+    }
+}
