ec402e0040333c25912c37ad6e2c792a07f50164
kay <56635816+nullotte@users.noreply.github.com>
2026-02-22 02:23:46 +0700

    payloadburner stats

--- src/carpediem/world/blocks/payloads/PayloadBurner.java
+++ src/carpediem/world/blocks/payloads/PayloadBurner.java
@@ -2,23 +2,69 @@ package carpediem.world.blocks.payloads;
 
 import arc.graphics.g2d.*;
 import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
 import arc.util.io.*;
+import carpediem.world.meta.*;
+import mindustry.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import mindustry.ui.*;
 import mindustry.world.*;
 import mindustry.world.blocks.payloads.*;
 import mindustry.world.draw.*;
+import mindustry.world.meta.*;
 
 // i eata da payload
 public class PayloadBurner extends PayloadBlock {
     public Block consumedBlock;
-    public float burnDuration = 60f * 60f;
+    public float burnDuration = 75f * 60f;
 
     public DrawBlock topDrawer;
 
     public PayloadBurner(String name) {
         super(name);
         acceptsPayload = true;
+    }
+
+    @Override
+    public void setStats() {
+        super.setStats();
+
+        stats.add(Stat.input, StatValues.blocks(Seq.with(consumedBlock)));
+        stats.add(Stat.productionTime, burnDuration / 60f, StatUnit.seconds);
+
+        // apparently i never properly implemented the processing types... this is going to come back to kill me in the future
+        // anyways this code is ripped straight from the reconstructor
+        Seq<ProcessableBlock> processableBlocks = Vars.content.blocks().select(b -> b instanceof ProcessableBlock).as();
+        stats.add(CDStat.recipes, table -> {
+            table.row();
+            for (ProcessableBlock block : processableBlocks) {
+                table.table(Styles.grayPanel, t -> {
+                    t.left();
+                    t.image(block.uiIcon).size(40).pad(10f).left().scaling(Scaling.fit).with(i -> StatValues.withTooltip(i, block));
+                    t.table(info -> {
+                        info.add(block.localizedName).left();
+                        info.row();
+                    }).pad(10).left();
+                }).fill().padTop(5).padBottom(5);
+
+                table.table(Styles.grayPanel, t -> {
+                    t.image(Icon.right).color(Pal.darkishGray).size(40).pad(10f);
+                }).fill().padTop(5).padBottom(5);
+
+                table.table(Styles.grayPanel, t -> {
+                    t.left();
+                    t.image(block.resultBlock.uiIcon).size(40).pad(10f).right().scaling(Scaling.fit).with(i -> StatValues.withTooltip(i, block.resultBlock));
+                    t.table(info -> {
+                        info.add(block.resultBlock.localizedName).right();
+                        info.row();
+                    }).pad(10).right();
+                }).fill().padTop(5).padBottom(5);
+
+                table.row();
+            }
+        });
     }
 
     @Override
