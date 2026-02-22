e2d259d7a69c1c5b9a90d824ee5a40d029d2a517
kay <56635816+nullotte@users.noreply.github.com>
2026-02-23 05:57:27 +0700

    always show these in the database

--- src/carpediem/world/blocks/payloads/SyntheticBlock.java
+++ src/carpediem/world/blocks/payloads/SyntheticBlock.java
@@ -1,12 +1,22 @@
 package carpediem.world.blocks.payloads;
 
+import mindustry.*;
 import mindustry.world.*;
 
-// its just solid and destructible = true
 public class SyntheticBlock extends Block {
     public SyntheticBlock(String name) {
         super(name);
         solid = true;
         destructible = true;
+    }
+
+    @Override
+    public boolean isHidden() {
+        return false;
+    }
+
+    @Override
+    public boolean isVisible() {
+        return !super.isHidden() && (Vars.state.rules.editor || (!Vars.state.rules.hideBannedBlocks || !Vars.state.rules.isBanned(this)));
     }
 }
