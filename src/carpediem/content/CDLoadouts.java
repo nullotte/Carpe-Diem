d37af4931377448d93b1d019ff95b28ef0df0b5e
kay <56635816+nullotte@users.noreply.github.com>
2026-03-06 18:10:57 +0700

    schem thing that might break everything

--- src/carpediem/content/CDLoadouts.java
+++ src/carpediem/content/CDLoadouts.java
@@ -14,7 +14,7 @@ public class CDLoadouts {
         schemLandingPodT0 = Schematics.readBase64("bXNjaAF4nBXKQQoCMQxA0YwWN57AXS5QmCu48w7iIp2GIZAmpQ3I3N4RPm/1IUG6QjJqDKgygw3D8cUU+Czmo5FiOVAO/xLcK89tSA9xA4CbUmGdcHl/FnhsNDrnKtyyklWxPXevOdbzXP6d/AALRx62");
 
         Vars.schematics.getLoadouts(CDStorage.landingPodT0).add(schemLandingPodT0);
-        // lalalalala
-        Reflect.<ObjectMap<CoreBlock, Schematic>>get(Vars.schematics, "defaultLoadouts").put(CDStorage.landingPodT0, schemLandingPodT0);
+        // no clue if this is necessary or not, and it seems to cause a crash with scheme size so i guess i'll get rid of it for now
+        // Reflect.<ObjectMap<CoreBlock, Schematic>>get(Vars.schematics, "defaultLoadouts").put(CDStorage.landingPodT0, schemLandingPodT0);
     }
 }
