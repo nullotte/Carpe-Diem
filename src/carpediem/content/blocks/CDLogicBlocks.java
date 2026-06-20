fe15aecef0dac0ce7c6a056ce793ef889be5d141
kay <56635816+nullotte@users.noreply.github.com>
2026-06-20 15:30:56 +0700

    item requirements for logic blocks

--- src/carpediem/content/blocks/CDLogicBlocks.java
+++ src/carpediem/content/blocks/CDLogicBlocks.java
@@ -4,6 +4,7 @@ import carpediem.content.*;
 import carpediem.graphics.*;
 import carpediem.world.blocks.logic.*;
 import carpediem.world.draw.*;
+import mindustry.content.*;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.logic.*;
@@ -19,7 +20,13 @@ public class CDLogicBlocks {
     public static void load() {
         computationProcessor = new DrawerLogicBlock("computation-processor") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    CDItems.aluminum, 100,
+                    CDItems.aluminumPlate, 50,
+                    CDItems.nickelWire, 70,
+                    CDItems.nickelPlate, 50,
+                    CDItems.controlCircuit, 30,
+                    CDItems.calculationCircuit, 50,
+                    Items.silicon, 50
             ));
             size = 3;
             instructionsPerTick = 25;
@@ -35,7 +42,9 @@ public class CDLogicBlocks {
 
         computationMessage = new DrawerMessageBlock("computation-message") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    Items.plastanium, 5,
+                    Items.silicon, 5,
+                    CDItems.calculationCircuit, 5
             ));
             drawer = new DrawMulti(
                     new DrawDefault(),
@@ -48,20 +57,30 @@ public class CDLogicBlocks {
 
         lever = new Lever("lever") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    CDItems.aluminumRod, 5,
+                    CDItems.aluminumCogwheel, 5,
+                    Items.silicon, 5,
+                    CDItems.calculationCircuit, 1
             ));
         }};
 
         memoryModule = new MemoryBlock("memory-module") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    CDItems.aluminum, 10,
+                    CDItems.nickelPlate, 10,
+                    Items.silicon, 10,
+                    CDItems.calculationCircuit, 5
             ));
             memoryCapacity = 64;
         }};
 
         largeMemoryModule = new MemoryBlock("large-memory-module") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    CDItems.aluminum, 30,
+                    CDItems.nickelPlate, 25,
+                    CDItems.nickelRod, 10,
+                    Items.silicon, 20,
+                    CDItems.calculationCircuit, 15
             ));
             memoryCapacity = 512;
             size = 2;
@@ -69,7 +88,10 @@ public class CDLogicBlocks {
 
         computationDisplay = new CDLogicDisplay("computation-display") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    CDItems.aluminum, 80,
+                    CDItems.plastaniumSheet, 50,
+                    Items.silicon, 30,
+                    CDItems.calculationCircuit, 5
             ));
             size = 3;
             displaySize = (size * 32) - 16;
@@ -77,7 +99,10 @@ public class CDLogicBlocks {
 
         largeComputationDisplay = new CDLogicDisplay("large-computation-display") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    CDItems.aluminum, 140,
+                    CDItems.plastaniumSheet, 80,
+                    Items.silicon, 50,
+                    CDItems.calculationCircuit, 15
             ));
             size = 6;
             displaySize = (size * 32) - 16;
@@ -85,7 +110,10 @@ public class CDLogicBlocks {
 
         tiledComputationDisplay = new TileableLogicDisplay("tiled-computation-display") {{
             requirements(Category.logic, ItemStack.with(
-                    CDItems.lemon, 39
+                    CDItems.aluminum, 5,
+                    CDItems.plastaniumSheet, 5,
+                    Items.silicon, 3,
+                    CDItems.calculationCircuit, 1
             ));
             frameSize = 8;
         }};
